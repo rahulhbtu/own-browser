054d38554c58898c0a6e339c1cbb7d7f5ed88440
Rahul Verma <95970334+rahulhbtu@users.noreply.github.com>
2023-10-26 13:07:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,55 @@
 import sys
-from PyQt5.QtCore import  *
-from PyQt5.QtWidgets import*
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 
 
 class MainWindow(QMainWindow):
-    def__init__(self):
+    def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl('http://google.com'))
         self.setCentralWidget(self.browser)
         self.showMaximized()
-    # navigation bar
-    navbar = QToolBar()
-    self.addToolbar(navbar)
 
-    back_btn= QAction('Back' , self)
-    back_btn.triggered.connect(self.browser.back)
-    navbar.addAction(back_btn)
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
 
-    forward_btn = QAction('Forward', self)
-    forward_btn.triggered.connect(self.browser.forward)
-    navbar.addAction(forward_btn)
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
 
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
 
-    reload_btn = QAction('Reload' , self)
-    reload_btn.triggered.connect(self.browser.reload)
-    navbar.addAction(reload_btn)
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
 
-    home_btn = QAction('Home',self)
-    home_btn.triggered.connect(self.navigate_home)
-    navbar.addAction(home_btn)
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
 
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
 
+        self.browser.urlChanged.connect(self.update_url)
 
-    self.url_bar = QLineEdit()
-    self.url_bar.returnPressed.connect(self.navigate_to_url)
-    navbar.addWidget(self.url_bar)
-    self.addWidget(self.url_bar)
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('http://programming-hero.com'))
 
-    self.browser.urlChanged.connect(self.update_url)
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
 
-
- def navigate_home(self):
-     self.browser.setUrl(QUrl('http://google.com'))
-
-
- def navigate_to_url(self):
-     url = self.url_bar.text()
-     self.browser.setUrl(QUrl(url))
-
-def update_url(self,q):
-    self.url_bar.setText(q.toString())
-
-
-
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
 
 
 app = QApplication(sys.argv)
-QApplication.setApplicationName('Rahul chrome')
+QApplication.setApplicationName('My Cool Browser')
 window = MainWindow()
-
 app.exec_()
-
-
